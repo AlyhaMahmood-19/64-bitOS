56c6ee9771e5490ce84fc2da20071c16a35e6fc9
AlyhaMahmood-19 <82828640+AlyhaMahmood-19@users.noreply.github.com>
2021-04-19 20:47:24 +0500

    Add files via upload

---
+++ os-series/src/impl/kernel/main.c
@@ -0,0 +1,30 @@
+#include "print.h"
+
+void kernel_main() {
+    print_clear();
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("Alyha Mahmood  291991  BS-CS-9B");
+    print_newline();
+    print_newline();
+    print_str("                *     *  *   *   ******  *******");
+    print_newline();
+    print_str("                **    *  *   *   *          *");
+    print_newline();
+    print_str("                * *   *  *   *   ******     *");
+    print_newline();
+    print_str("                *  *  *  *   *        *     *");
+    print_newline();
+    print_str("                *    **  *****  *******     *");
+    print_newline();
+    print_str("<<<----------*************************************---------->>>");
+    print_newline();
+    print_str("              @@@@@@  @@@@@  @@@@@  @@@@@  @@@@@@");
+    print_newline();
+    print_str("              @       @      @      @      @");
+    print_newline();
+    print_str("              @@@@@@  @@@@@  @      @      @@@@@@");
+    print_newline();
+    print_str("                   @  @      @      @           @");
+    print_newline();
+    print_str("              @@@@@@  @@@@@  @@@@@  @@@@@  @@@@@@");
+}
